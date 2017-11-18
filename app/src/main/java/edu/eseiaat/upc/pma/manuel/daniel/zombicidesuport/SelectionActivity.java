345b389944416737e33dddcb0efc30028ef2fb2a
Danieldbc <daniel.dbc01@gmail.com>
2017-11-18 13:58:55 +0100

    Version 1.04

--- app/src/main/java/edu/eseiaat/upc/pma/manuel/daniel/zombicidesuport/SelectionActivity.java
+++ app/src/main/java/edu/eseiaat/upc/pma/manuel/daniel/zombicidesuport/SelectionActivity.java
@@ -4,9 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SelectionActivity extends AppCompatActivity {
 
@@ -15,6 +16,7 @@ public class SelectionActivity extends AppCompatActivity {
     private PersonajesAdapter adapter;
     private LinearLayoutManager linlayoutmanager;
     private ImageView descripcionPersonaje;
+    private TextView habAzul,habAmarilla, habNaranja1, habNaranja2, habRoja1, habRoja2,habRoja3;
 
 
     @Override
@@ -23,6 +25,13 @@ public class SelectionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_selection);
 
         descripcionPersonaje =(ImageView)findViewById(R.id.DescripcionPersonaje);
+        habAzul=(TextView) findViewById(R.id.HabAzul);
+        habAmarilla=(TextView)findViewById(R.id.HabAmarilla);
+        habNaranja1=(TextView) findViewById(R.id.HabNaranja1);
+        habNaranja2=(TextView) findViewById(R.id.HabNaranja2);
+        habRoja1=(TextView) findViewById(R.id.HabRoja1);
+        habRoja2=(TextView) findViewById(R.id.HabRoja2);
+        habRoja3=(TextView) findViewById(R.id.HabRoja3);
 
         viewPersonajes =(RecyclerView)findViewById(R.id.ListaPersonajes);
         linlayoutmanager =new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,true);
@@ -31,6 +40,7 @@ public class SelectionActivity extends AppCompatActivity {
         viewPersonajes.setAdapter(adapter);
 
         CrearPersonajes();
+        Watts();
 
         adapter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,21 +57,106 @@ public class SelectionActivity extends AppCompatActivity {
     private void PersonajeSeleccionado(View view) {
         int d=viewPersonajes.getChildAdapterPosition(view);
 
-        if (d==3){
-            descripcionPersonaje.setImageDrawable(getDrawable(R.drawable.calavera));
+        if (d==0){
+            Watts();
         }
+        if (d==1){
+            Joshua();
+        }
+        if (d==2){
+            Belle();
+        }
+        if (d==3){
+            Grindlock();
+        }
+        if (d==4){
+            Kim();
+        }
+        if (d==5){
+            Shannon();
+        }
+    }
+
+    private void Shannon() {
+        descripcionPersonaje.setImageDrawable(getDrawable(R.drawable.pshannon));
+        habAzul.setText(R.string.DisparoABocajarro);
+        habAmarilla.setText(R.string.mas1accion);
+        habNaranja1.setText(R.string.mas1accionADistanciaGratuita);
+        habNaranja2.setText(R.string.Afortunada);
+        habRoja1.setText(R.string.mas1dadoCombate);
+        habRoja2.setText(R.string.mas1accionDeCombateGratuita);
+        habRoja3.setText(R.string.Escurridiza);
+    }
+
+    private void Kim() {
+        descripcionPersonaje.setImageDrawable(getDrawable(R.drawable.pkim));
+        habAzul.setText(R.string.Afortunada);
+        habAmarilla.setText(R.string.mas1accion);
+        habNaranja1.setText(R.string.seisEnElDadoMas1DadoADistancia);
+        habNaranja2.setText(R.string.Amano);
+        habRoja1.setText(R.string.mas1accionDeCombateGratuita);
+        habRoja2.setText(R.string.mas1alasTiradasDeCombate);
+        habRoja3.setText(R.string.seisEnElDadoMas1DadoCuerpoACuerpo);
+    }
+
+    private void Grindlock() {
+        descripcionPersonaje.setImageDrawable(getDrawable(R.drawable.pgrindlock));
+        habAzul.setText(R.string.Provocacion);
+        habAmarilla.setText(R.string.mas1accion);
+        habNaranja1.setText(R.string.mas1accionDeCombateCuerpoACuerpoGratuita);
+        habNaranja2.setText(R.string.Escurridizo);
+        habRoja1.setText(R.string.mas1AlDañoCuerpoACuerpo);
+        habRoja2.setText(R.string.EsoEsTodoLoQueTienes);
+        habRoja3.setText(R.string.seisEnElDadoMas1DadoDeCombate);
+    }
+
+    private void Belle() {
+        descripcionPersonaje.setImageDrawable(getDrawable(R.drawable.pbelle));
+        habAzul.setText(R.string.mas1accionDeMovimientoGratuita);
+        habAmarilla.setText(R.string.mas1accion);
+        habNaranja1.setText(R.string.mas1aLasTiradasADistancia);
+        habNaranja2.setText(R.string.mas1accionDeCombateCuerpoACuerpoGratuita);
+        habRoja1.setText(R.string.mas1dadoCombate);
+        habRoja2.setText(R.string.mas1accionDeMovimientoGratuita);
+        habRoja3.setText(R.string.Ambidiestra);
+    }
+
+    private void Watts() {
+        descripcionPersonaje.setImageDrawable(getDrawable(R.drawable.pwatts));
+        habAzul.setText(R.string.EmpezarConBateBeisbol);
+        habAmarilla.setText(R.string.mas1accion);
+        habNaranja1.setText(R.string.mas1dadoCuerpoACuerpo);
+        habNaranja2.setText(R.string.Empujon);
+        habRoja1.setText(R.string.dosZonasPorAccionDeMovimiento);
+        habRoja2.setText(R.string.mas1accionDeCombateGratuita);
+        habRoja3.setText(R.string.mas1alasTiradasDeCombate);
+    }
+
+    private void Joshua() {
+
+        descripcionPersonaje.setImageDrawable(getDrawable(R.drawable.pjoshua));
+        habAzul.setText(R.string.Socorrista);
+        habAmarilla.setText(R.string.mas1accion);
+        habNaranja1.setText(R.string.mas1acciónDeCombateADistancia);
+        habNaranja2.setText(R.string.mas1aLasTiradasCuerpoACuerpo);
+        habRoja1.setText(R.string.mas1accionDeCombateGratuita);
+        habRoja2.setText(R.string.mas1aLasTiradasADistancia);
+        habRoja3.setText(R.string.LiderNato);
     }
 
     private void CrearPersonajes() {
 
-        adapter.add(new personajes("hola",getDrawable(R.drawable.calavera)));
-        adapter.add(new personajes("hola2",getDrawable(R.drawable.tabla)));
-        adapter.add(new personajes("hola3",getDrawable(R.drawable.ejemplo)));
-        adapter.add(new personajes("hola4",getDrawable(R.drawable.rifle)));
-        adapter.add(new personajes("hola5",getDrawable(R.drawable.madera)));
-        adapter.add(new personajes("hola6",getDrawable(R.drawable.ladrillos)));
-        adapter.add(new personajes("hola7",getDrawable(R.drawable.mancharoja)));
-        adapter.add(new personajes("hola8",getDrawable(R.drawable.calavera)));
+        adapter.add(new personajes("Watts",getDrawable(R.drawable.pwattscara)));
+        adapter.add(new personajes("Joshua",getDrawable(R.drawable.pjoshuacara)));
+        adapter.add(new personajes("Belle",getDrawable(R.drawable.pbellecara)));
+        adapter.add(new personajes("Grindlock",getDrawable(R.drawable.pgrindlockcara)));
+        adapter.add(new personajes("Kim",getDrawable(R.drawable.pkimcara)));
+        adapter.add(new personajes("Shannon",getDrawable(R.drawable.pshannoncara)));
+        //sobran los siquientes(en el futuro habrá más personajes), son para ver si funciona el recyclerview
+        adapter.add(new personajes("Grindlock",getDrawable(R.drawable.pgrindlockcara)));
+        adapter.add(new personajes("Kim",getDrawable(R.drawable.pkimcara)));
+        adapter.add(new personajes("Shannon",getDrawable(R.drawable.pshannoncara)));
+
     }
 
 
